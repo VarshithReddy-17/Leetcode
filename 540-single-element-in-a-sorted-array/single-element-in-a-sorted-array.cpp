4b1d147744d697164c0d333d9eaeb38f59f58c5c
VarshithReddy-17 <varshithreddymamilla17@gmail.com>
2026-03-17 14:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (92.58%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size() - 1;
-        int ans = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int low=0;
+        int high=nums.size()-1;
+        int ans=-1;
+        while (low<=high) {
+            int mid=(low+high)/2;
 
-            int i = mid;
-            int j = mid;
-            while (i >= 0) {
-                if (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+            int i=mid;
+            int j=mid;
+            while (i>=0) {
+                if(i+1<nums.size()&&nums[i]==nums[i+1]) {
                     i--;
                 }
-                else if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
+                else if(i-1>=0&&nums[i]==nums[i-1]) {
                     i--;
                 }
                 else {
-                    ans = nums[i];
+                    ans=nums[i];
                     break;
                 }
             }
-            while (j < nums.size()) {
-                if (j - 1 >= 0 && nums[j] == nums[j - 1]) {
+            while (j<nums.size()) {
+                if (j-1>=0&&nums[j]==nums[j-1]) {
                     j++;
                 }
-                else if (j + 1 < nums.size() && nums[j] == nums[j + 1]) {
+                else if (j+1<nums.size()&&nums[j]==nums[j+1]) {
                     j++;
                 }
                 else {
-                    ans = nums[j];
+                    ans=nums[j];
                     break;
                 }
             }
